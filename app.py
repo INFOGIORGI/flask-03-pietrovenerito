d456f4a8e12e7b57c29228462e5fec26af1656cb
pietrovenerito <pietrovenerito28@gmail.com>
2024-12-13 12:36:09 +0000

    2 pagine web

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template  
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html",titolo='Home Page')
+
+@app.route("/dettagli")
+def detail():
+    prodotti = (("Laptop","6","50€"), ("telefono","5","50€"),("computer","2","1000€"))
+    return render_template("detail.html",titolo='Dettagli',prodotti=prodotti)
 
 app.run()
