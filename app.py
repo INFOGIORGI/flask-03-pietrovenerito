d08d8e876bcbd069ba7a0de9166b0373c816d648
pietrovenerito <pietrovenerito28@gmail.com>
2024-12-17 11:12:55 +0000

    esercizio 0.3 finito

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template  
 
 app = Flask(__name__)
+prodotti = (("Laptop","6","50€"), ("telefono","5","50€"),("computer","2","1000€"))
 
 @app.route("/")
 def home():
@@ -8,7 +9,16 @@ def home():
 
 @app.route("/dettagli")
 def detail():
-    prodotti = (("Laptop","6","50€"), ("telefono","5","50€"),("computer","2","1000€"))
     return render_template("detail.html",titolo='Dettagli',prodotti=prodotti)
+
+@app.route("/scaffale/<numscaffale>")
+def scaffale(numscaffale):
+    l=[]
+    for t in prodotti:
+        if t[1]==numscaffale:
+            l.append(t)
+    return render_template("scaffale.html",titolo='Scaffale',numscaffale=numscaffale,prodotti=l)
+
+
 
 app.run()
